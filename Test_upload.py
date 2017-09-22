c6f562433ee19f37e0bdd9adb3976723116f41e6
Ashok Shetty <ashu.shetty14@gmail.com>
2017-09-22 10:01:29 +0530

    Add files via upload

---
+++ Test_upload.py
@@ -0,0 +1,80 @@
+
+# coding: utf-8
+
+# In[2]:
+
+a = int(input("Enter Temperature:"))
+
+
+# In[18]:
+
+cm = float(input("Enter cm vlaue:"))
+inches = 0.394*cm
+feet = 0.328*cm
+if cm > 0:
+    print("inches is equal to %d and feet is equal to %d" %(inches,feet))
+else:
+    print("Enter correct input")
+    
+
+
+# In[26]:
+
+n=int(input("Enter an integer:"))
+a=[]
+for i in range(2,n+1):
+    if(n%i==0):
+        a.append(i)
+a.sort()
+print("Smallest divisor is:",a[0])
+
+
+# In[31]:
+
+n = int(input("Enter an integer:"))
+a = []
+for i in range(2,n+1) :
+    if(i%)
+    
+
+
+# In[35]:
+
+x = range(2,20)
+print(x)
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
+
+# In[ ]:
+
+
+
